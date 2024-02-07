972e6b1ff74474a316b24c4143a4625bdbca9986
Abudadev <132628423+Abudadev@users.noreply.github.com>
2024-02-06 18:36:33 -0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,49 @@
+objects = [];
+
+function preload(){
+    petimg = loadImage("Rizzcat.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+    ObjectDetech = ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("Status").innerHTML = "Status: Detecting for objects";
+}
+
+function draw(){
+    image(petimg,0,0,500,500);
+        if(status != ""){
+            document.getElementById("Status").innerHTML = "Status: Objects Detected";
+            for(i = 0; i < objects.length; i++){
+
+             fill("black");
+             percent = Math.floor(objects[i].confidence * 100);
+             text(objects[i].label + " " + percent + "%",objects[i].x,objects[i].y);
+             noFill();
+             stroke("black");
+             rect(objects[i].x,objects[i].y,objects[i].width + 15,objects[i].height + 15);
+            }
+        }
+
+}
+
+function back(){
+    window.location = "index.html";
+}
+
+function modelloaded(){
+    console.log("Model has loaded!");
+    status = true;
+    ObjectDetech.detect(petimg,gotresults);
+}
+
+function gotresults(error,results){
+ if(error){
+ console.error(error);
+}
+else{
+    console.log(results);
+    objects = results;
+}
+}
---
+++ main3.js
@@ -0,0 +1,48 @@
+objects = [];
+
+function preload(){
+    Toy = loadImage("toy.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+    ObjectDetech = ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("Status").innerHTML = "Status: Detecting for objects";
+}
+
+function draw(){
+    image(Toy,0,0,500,500);
+    if(status != ""){
+        document.getElementById("Status").innerHTML = "Status: Objects Detected";
+        for(i = 0; i < objects.length; i++){
+         fill("black");
+         percent = Math.floor(objects[i].confidence * 100);
+         text(objects[i].label + " " + percent + "%",objects[i].x,objects[i].y);
+         noFill();
+         stroke("black");
+         rect(objects[i].x,objects[i].y,objects[i].width + 15,objects[i].height + 15);
+        }
+    }
+}
+
+function back(){
+    window.location = "index.html";
+}
+
+function modelloaded(){
+    console.log("Model has loaded!");
+    status = true;
+    ObjectDetech.detect(Toy,gotresults);
+}
+
+
+function gotresults(error,results){
+    if(error){
+    console.error(error);
+   }
+   else{
+       console.log(results);
+       objects = results;
+   }
+}
